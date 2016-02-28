ec29d26a0471e3b2be4dd36ac803f288168442b3
Hugh Grigg <hugh.grigg@gmail.com>
2016-02-28 20:44:32 +0000

    Test image internal check

---
+++ tests/Unit/ChingShop/Image/ImageTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Testing\Unit\ChingShop\Image;
+
+use Testing\Unit\UnitTest;
+use ChingShop\Image\Image;
+
+class ImageTest extends UnitTest
+{
+    /** @var Image */
+    private $image;
+
+    /**
+     * Set up image for each test
+     */
+    public function setUp()
+    {
+        $this->image = new Image;
+    }
+
+    /**
+     * Sanity check for instantiation
+     */
+    public function testConstruct()
+    {
+        $this->assertInstanceOf(Image::class, $this->image);
+    }
+
+    /**
+     * Is internal should be true if the image has a filename
+     */
+    public function testIsInternal()
+    {
+        $this->image->filename = 'foo file';
+        $this->assertTrue($this->image->isInternal());
+    }
+}
