e8bd79494957c2d35c2f5369d972a8e0bba89548
Hugh Grigg <hugh.grigg@gmail.com>
2016-07-11 20:53:21 +0100

    Fix jQuery UI Sortable path

--- resources/assets/ts/src/staff.ts
+++ resources/assets/ts/src/staff.ts
@@ -1,6 +1,6 @@
 require("./main.js");
 
-require("../../../node_modules/jquery-ui/sortable.js");
+require("../../../node_modules/jquery-ui/ui/widgets/sortable.js");
 require("../../../node_modules/bootstrap-multiselect/dist/js/bootstrap-multiselect.js");
 
 require("./staff/product.js");
