3f5ce59b32e9bffd4b36163fb937ad0c4818a62c
Hugh Grigg <hugh.grigg@gmail.com>
2016-09-04 18:10:00 +0100

    Fix Vagrant provisioning

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,18 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ ! -d ~/.bash_it ]; then
-    echo 'Installing bash-it'
-    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-    ~/.bash_it/install.sh
-    sed -i -e 's/bobby/nwinkler/' ~/.bashrc
-fi
-
-if ! grep -q 'gulp --completion' ~/.bashrc; then
-    echo $'\n# Gulp tab completion' >> ~/.bashrc
-    echo 'eval "$(gulp --completion=bash)"' >> ~/.bashrc
-fi
-
 if ! grep -q 'EDITOR' ~/.bashrc; then
     echo "export EDITOR=vim" >> ~/.bashrc
 fi
@@ -26,21 +14,43 @@ if ! grep -q 'COMPOSER_DISABLE_XDEBUG_WARN' ~/.bashrc; then
     echo "export COMPOSER_DISABLE_XDEBUG_WARN=1" >> ~/.bashrc
 fi
 
+if ! grep -q '.composer/vendor/bin' ~/.bashrc; then
+    export PATH=$PATH:$HOME/.composer/vendor/bin/
+    echo 'export PATH=$PATH:$HOME/.composer/vendor/bin/' >> ~/.bashrc
+fi
+
+source $HOME/.bashrc
+
 git config --global core.excludesfile ~/.gitignore_global
 echo '.idea' > ~/.gitignore_global
+
+composer global require "squizlabs/php_codesniffer=^2.5" --quiet
+composer global require "phpmd/phpmd=@stable" --quiet
 
 function appSetup
 {
     cd ~/ching-shop
     cp -n .env.example .env
-    composer install
+    composer install --quiet --no-interaction
     bundler install
     php ~/ching-shop/artisan config:clear
     php artisan key:generate
     php artisan migrate --seed
-    npm install
-    gulp
+    npm install --silent
+    gulp --silent
     phpcs --config-set colors 1
     phpcs --config-set severity 1
 }
 appSetup
+
+if [ ! -d ~/.bash_it ]; then
+    echo 'Installing bash-it'
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    ~/.bash_it/install.sh
+    sed -i -e 's/bobby/nwinkler/' ~/.bashrc
+fi
+
+if ! grep -q 'gulp --completion' ~/.bashrc; then
+    echo $'\n# Gulp tab completion' >> ~/.bashrc
+    echo 'eval "$(gulp --completion=bash)"' >> ~/.bashrc
+fi
