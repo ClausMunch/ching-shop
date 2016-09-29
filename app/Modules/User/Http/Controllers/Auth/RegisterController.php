bbd92f702928f8fbbe89c0bca63194e5bb340992
Hugh Grigg <hugh.grigg@gmail.com>
2016-09-29 23:26:00 +0100

    Reformat new Laravel 5.3 classes

--- app/Modules/User/Http/Controllers/Auth/RegisterController.php
+++ app/Modules/User/Http/Controllers/Auth/RegisterController.php
@@ -51,11 +51,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'name'     => 'required|max:255',
-            'email'    => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
+        return Validator::make(
+            $data,
+            [
+                'name'     => 'required|max:255',
+                'email'    => 'required|email|max:255|unique:users',
+                'password' => 'required|min:6|confirmed',
+            ]
+        );
     }
 
     /**
@@ -67,10 +70,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name'     => $data['name'],
-            'email'    => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        return User::create(
+            [
+                'name'     => $data['name'],
+                'email'    => $data['email'],
+                'password' => bcrypt($data['password']),
+            ]
+        );
     }
 }
