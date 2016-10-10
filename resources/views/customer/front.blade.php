c2e2636b121e179bb140243fa1206a669a5fb443
Hugh Grigg <hugh.grigg@gmail.com>
2016-10-10 22:31:03 +0100

    Display postage

--- resources/views/customer/front.blade.php
+++ resources/views/customer/front.blade.php
@@ -34,5 +34,21 @@
 
     </div>
 
-@endsection
+    <div class="container-fluid">
+        <div class="row top-points">
+            <div class="col-sm-6 col-md-4 top-points-item">
+                <span class="icon icon-truck"></span>&nbsp;
+                Free UK Delivery
+            </div>
+            <div class="col-sm-6 col-md-4 top-points-item">
+                <span class="icon icon-esc"></span>&nbsp;
+                Easy Returns
+            </div>
+            <div class="col-sm-6 col-md-4 top-points-item">
+                <span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;
+                Great Customer Service
+            </div>
+        </div>
+    </div>
 
+@endsection
