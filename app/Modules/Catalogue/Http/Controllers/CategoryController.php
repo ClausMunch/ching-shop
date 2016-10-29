8974599dbb482dcd3729063f8f53ee8c842b875f
Hugh Grigg <hugh.grigg@gmail.com>
2016-10-29 13:01:09 +0100

    Display sub-category products

--- app/Modules/Catalogue/Http/Controllers/CategoryController.php
+++ app/Modules/Catalogue/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace ChingShop\Modules\Catalogue\Http\Controllers;
 use ChingShop\Http\Controllers\Controller;
 use ChingShop\Http\WebUi;
 use ChingShop\Modules\Catalogue\Domain\Category;
+use ChingShop\Modules\Catalogue\Domain\Product\Product;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
@@ -16,16 +17,24 @@ class CategoryController extends Controller
     /** @var Category */
     private $category;
 
+    /** @var Product */
+    private $product;
+
     /** @var WebUi */
     private $webUi;
 
     /**
      * @param Category $category
+     * @param Product  $product
      * @param WebUi    $webUi
      */
-    public function __construct(Category $category, WebUi $webUi)
-    {
+    public function __construct(
+        Category $category,
+        Product $product,
+        WebUi $webUi
+    ) {
         $this->category = $category;
+        $this->product = $product;
         $this->webUi = $webUi;
     }
 
@@ -40,9 +49,7 @@ class CategoryController extends Controller
     public function viewCategory(int $id, string $slug)
     {
         /** @var Category $category */
-        $category = $this->category->with(['products'])->findOrFail(
-            Category::privateId($id)
-        );
+        $category = $this->category->findOrFail(Category::privateId($id));
 
         if ($category->slug() !== $slug) {
             return $this->webUi->redirect(
@@ -51,9 +58,19 @@ class CategoryController extends Controller
             );
         }
 
+        $tree = $category->getDescendantsAndSelf()
+            ->load(
+                [
+                    'products' => function ($query) {
+                        /** @var Product $query */
+                        $query->with(Product::standardRelations());
+                    },
+                ]
+            );
+
         return $this->webUi->view(
             'customer.category.view',
-            compact('category')
+            compact('category', 'tree')
         );
     }
 }
